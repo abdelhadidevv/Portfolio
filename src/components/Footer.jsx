1c8d484074cc45e050af5bbcb08aafd9ffc56c9b
Abdelhady Abdelall <aabd71626@gmail.com>
2022-03-14 10:58:05 +0200

    fix bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
       <p>
         © 2022 Abd Elhadi. All Rights Reserved to<span>&nbsp;</span>
       </p>
-      <a href="https://www.craftz.dog/" target="_blank" className="craftz">
+      <a href="https://www.craftz.dog/" target="_blank" className="craftz" rel="noreferrer">
         Craftz Dog.
       </a>
     </div>
